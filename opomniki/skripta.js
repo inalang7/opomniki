32eec9cef6e990e663a25ae57ce3e73b91708a1f
Ina Lang <ina_lang@live.com>
2016-08-31 13:22:29 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -8,11 +8,14 @@ window.addEventListener('load', function() {
 	};
 	document.querySelector("#prijavniGumb").addEventListener('click', pridobijIme);
 	var nastavitveOpomnikov = function() {
+		
 		var cas_opomnika = document.querySelector("#cas_opomnika").value;
-		var naziv = document.querySelector("#naziv_opomnika").value;
-		document.getElementById("cas_opomnika").value="";
-		document.getElementById("naziv_opomnika").value="";
-		document.querySelector("#opomniki").innerHTML += " <div class='opomnik'><div class='naziv_opomnika'>"+naziv+"</div> <div class='cas_opomnika'> Opomnik čez <span>"+cas_opomnika+"</span> sekund.</div> </div>";
+		var naziv_opomnika = document.querySelector("#naziv_opomnika").value;
+		
+		document.querySelector("#cas_opomnika").value="";
+		document.querySelector("#naziv_opomnika").value="";
+		
+		document.querySelector("#opomniki").innerHTML += " <div class ='opomnik rob senca'><div class='naziv_opomnika'>"+naziv_opomnika+"</div> <div class='cas_opomnika'> Opomnik čez <span>"+cas_opomnika+"</span> sekund.</div> </div>";
 		
 	};
 	document.querySelector("#dodajGumb").addEventListener('click', nastavitveOpomnikov);
@@ -21,14 +24,19 @@ window.addEventListener('load', function() {
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
 		
-		for (i = 0; i < opomniki.length; i++) {
+		for (var i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
 	
-			//TODO: 
-			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
-			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			if(cas === 0){
+				window.alert("Opomnik!\n\nZadolžitev "+ opomnik.querySelector(".naziv_opomnika").innerHTML +" je potekla!")
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}else{
+				cas = cas-1;
+				casovnik.innerHTML = cas;
+ 			}
+		
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
